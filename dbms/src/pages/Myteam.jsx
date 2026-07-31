ccd067452b8edcfcdd150bd4d9a5d8249a16c0a6
/ <Shravanihire06@gmail.com>
2026-07-31 12:58:01 +0530

    fix: correct CSS import case for Render build

--- dbms/src/pages/Myteam.jsx
+++ dbms/src/pages/Myteam.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './MyTeam.css';
+import './Myteam.css';
 
 const Myteam = () => {
   const [team, setTeam] = useState([]);
